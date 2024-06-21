d9ba1bbe42d1c17edaf671c099614590f41af904
João Pedro <nmjoaopedro22@gmail.com>
2024-06-21 10:47:01 -0300

    feat: trata exceções de atualização de usuários

--- src/main/java/app/services/UserService.java
+++ src/main/java/app/services/UserService.java
@@ -4,6 +4,7 @@ import app.entities.User;
 import app.repositories.UserRepository;
 import app.services.exceptions.DatabaseException;
 import app.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -39,9 +40,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
