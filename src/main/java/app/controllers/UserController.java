64de8bc18c7747a8da1b52af27af34c49471ab13
João Pedro <nmjoaopedro22@gmail.com>
2024-06-21 09:37:10 -0300

    feat: implementa atualização de usuários

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -43,4 +43,10 @@ public class UserController {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = service.update(id, user);
+        return ResponseEntity.ok().body(user);
+    }
 }
