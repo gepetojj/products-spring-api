da7889525652095d60e8b3d82098e9007bcc040e
João Pedro <nmjoaopedro22@gmail.com>
2024-06-21 09:05:13 -0300

    feat: adiciona subtotal e total dos pedidos

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -85,6 +85,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal() {
+        Double sum = 0.0;
+        for (OrderItem item : items) {
+            sum += item.getPrice();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
